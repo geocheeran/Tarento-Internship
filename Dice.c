4fafbb321294a5ba3c4287e9a79288e40a3f2a0d
Geo Jose <32750937+geocheeran@users.noreply.github.com>
2020-07-29 19:02:32 +0530

    Update Dice.c

--- Dice.c
+++ Dice.c
@@ -2,40 +2,36 @@
 
 // Write a program to design a Dice game. There should be two players. Ask the users to enter their names and roll the dice(four times each). Display the scores and calculate the final score and print the name of the winner.
 
-#include<stdio.h>
-#include <stdlib.h>
-#include<math.h>
-int main()
-{
-	char p1[10],p2[10];
-	int s1=0,s2=0;
-	int q=0,r=0;
-    printf("Enter player 1 name \n");
-    scanf("%s",&p1);
-    printf("Enter player 2 name \n");
-    scanf("%s",&p2);
-    for (int i=0;i<4;i++)
-    {
-    	printf("chance of %s \n",&p1);
-    	q = rand() % 6;
-    	printf("You got %d \n",q);
-    	s1=s1+q;
-    	printf("chance of %s \n",&p2);
-    	r = rand() % 6;
-    	printf("You got %d \n",r);
-    	s2=s2+r;
-
-
-    }
-    printf("Total scores for %s is %d \n",&p1,s1);
-    printf("Total scores for %s is %d \n",&p2,s2);
-    printf("The winner is\n");
-    if(s1>s2)
-    	printf("%s",&p1);
-    else
-    	printf("%s",&p2);
-
-
-
+#include<iostream>
+ 
+using namespace std;
+ 
+int main(){
+	int p1=0,p2=0;
+	string pa,pb;
+	cout<<"Enter player 1 name : ";
+	getline(cin,pa);
+	cout<<"\nEnter player 2 name : ";
+	getline(cin,pb);
+	cout<<'\n';
+	int a;
+	for(int i=0;i<4;i++){
+		cout<<"Enter player 1 dice value : ";
+		cin>>a;
+		pa+=a;
+		cout<<'\n';
+	}
+	for(int i=0;i<4;i++){
+		cout<<"Enter player 2 dice value : ";
+		cin>>a;
+		pb+=a;
+		cout<<'\n';
+	}
+	if(pa==pb)
+		cout<<"Draw\n";
+	else if(pa>pb)
+		cout<<"Player 1 wins\n";
+	else
+		cout<<"Player 2 wins\n";
 	return 0;
 }
